a0f001dccad5820ba15dfc57426bafd0444ddaf1
gamingzeether <80543808+gamingzeether@users.noreply.github.com>
2022-02-14 18:25:29 -0800

    update includes

--- script_macros.hpp
+++ script_macros.hpp
@@ -1,3 +1,12 @@
 #define PREFIX minigames
 
-#include "includes\x\cba\addons\main\script_macros_common.hpp"
+#include "include\z\ace\addons\main\script_macros.hpp"
+
+#define ARR2(var1,var2)                                    [var1, var2]
+#define ARR3(var1,var2,var3)                               [var1, var2, var3]
+#define ARR4(var1,var2,var3,var4)                          [var1, var2, var3, var4]
+#define ARR5(var1,var2,var3,var4,var5)                     [var1, var2, var3, var4, var5]
+#define ARR6(var1,var2,var3,var4,var5,var6)                [var1, var2, var3, var4, var5, var6]
+#define ARR7(var1,var2,var3,var4,var5,var6,var7)           [var1, var2, var3, var4, var5, var6, var7]
+#define ARR8(var1,var2,var3,var4,var5,var6,var7,var8)      [var1, var2, var3, var4, var5, var6, var7, var8]
+#define ARR9(var1,var2,var3,var4,var5,var6,var7,var8,var9) [var1, var2, var3, var4, var5, var6, var7, var8, var9]
